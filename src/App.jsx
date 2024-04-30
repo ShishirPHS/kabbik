63c36ecc37e3bcfb9356cbb0aeba7883ee276963
Shishir-Ahmed <shishirahmed790@gmail.com>
2024-04-30 17:18:45 +0600

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,44 @@
+import Navbar from "./assets/components/Navbar/Navbar";
+
 function App() {
-  return <>Hello World</>;
+  return (
+    <>
+      <div className="drawer">
+        <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col">
+          {/* Navbar */}
+          <Navbar></Navbar>
+          {/* Page content here */}
+          Content
+        </div>
+        <div className="drawer-side">
+          <label
+            htmlFor="my-drawer-3"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <ul className="menu p-4 w-80 min-h-full bg-base-200 sideNav">
+            {/* Sidebar content here */}
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Products</a>
+            </li>
+            <li>
+              <a>Services</a>
+            </li>
+            <li>
+              <button>Login</button>
+            </li>
+            <li>
+              <a>Services</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/assets/components/Navbar/Navbar.jsx
@@ -0,0 +1,64 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="container mx-auto navbar flex justify-between">
+        <div className="flex flex-row-reverse justify-between w-full">
+          {/* sidebar opening button */}
+          <div className="flex-none lg:hidden">
+            <label
+              htmlFor="my-drawer-3"
+              aria-label="open sidebar"
+              className="btn btn-square btn-ghost"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block w-6 h-6 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </label>
+          </div>
+          <div className="flex-1">
+            <img src="../src/assets/navLogo.png" alt="Website Logo" />
+          </div>
+        </div>
+        {/* navbar for large devices */}
+        <div className="flex-none hidden lg:block largeNav">
+          <ul className="menu menu-horizontal pr-0 items-center">
+            {/* Navbar menu content here */}
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Products</a>
+            </li>
+            <li>
+              <a>Services</a>
+            </li>
+            <li>
+              <button className="px-[26px] py-[6px] bg-[#2F6F59] rounded-[20px] text-white">
+                Login
+              </button>
+            </li>
+            <li>
+              <button className="pr-0">
+                <img src="../src/assets/cartIcon.png" alt="icon of cart" />
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
