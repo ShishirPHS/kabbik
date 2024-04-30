66b3977bafd3929e76cef0ccaa77485869111bbc
Shishir-Ahmed <shishirahmed790@gmail.com>
2024-04-30 20:26:57 +0600

    lower banner done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,5 +1,8 @@
 import bannerRight from "../../assets/bannerRight.png";
 import allProdBtn from "../../assets/allProdIcon.png";
+import sofa from "../../assets/sofa.png";
+import chair from "../../assets/chair.png";
+import bed from "../../assets/bed.png";
 
 const Banner = () => {
   return (
@@ -27,7 +30,7 @@ const Banner = () => {
             </div>
             <div>
               <img
-                className="absolute right-[108px] top-0"
+                className="absolute right-[108px] top-0 z-10"
                 src={bannerRight}
                 alt="Banner Image"
               />
@@ -36,19 +39,56 @@ const Banner = () => {
         </div>
       </div>
       {/* banner lower */}
-      <div className="bg-[#FDCC1A] pt-[42px] pb-[60px]">
+      <div className="bg-[#FDCC1A] pt-[42px] pb-[60px] relative">
         <div className="container mx-auto">
-          <button
-            style={{
-              boxShadow: "0px 0px 15px 0px rgba(0, 57, 40, 0.82) inset",
-            }}
-            className="text-white py-2 px-7 rounded bg-[#2F6F59] font-fontNarrow"
+          <div className="flex">
+            <button
+              style={{
+                boxShadow: "0px 0px 15px 0px rgba(0, 57, 40, 0.82) inset",
+              }}
+              className="text-white py-2 px-7 rounded bg-[#2F6F59] font-fontNarrow mr-[35px]"
+            >
+              Buy Now
+            </button>
+            <button className="font-fontNarrow font-medium text-xl">
+              <span className="flex items-center">
+                <img src={allProdBtn} alt="" /> All Product
+              </span>
+            </button>
+          </div>
+        </div>
+        {/* cards */}
+        <div className="flex items-center space-x-[35px] absolute top-[35px] left-[50%] -translate-x-[50%] z-20">
+          {/* card 1 */}
+          <div
+            style={{ boxShadow: "0px 1px 13px 0px rgba(46, 49, 146, 0.09)" }}
+            className="pt-[43px] pb-[15px] px-3 bg-gradient-to-b from-[#BDEFDE] to-[#70CEB1] rounded-[22px] flex flex-col items-center"
           >
-            Buy Now
-          </button>
-          <button>
-            <img src={allProdBtn} alt="" /> All Product
-          </button>
+            <img src={sofa} alt="" />
+            <h4 className="font-semibold text-xl font-fontNarrow mt-[18px]">
+              Sofa
+            </h4>
+          </div>
+          {/* card 2 */}
+          <div
+            style={{ boxShadow: "0px 1px 13px 0px rgba(46, 49, 146, 0.09)" }}
+            className="pt-[14px] pb-[15px] px-[39px] bg-gradient-to-b from-[#BDEFDE] to-[#70CEB1] rounded-[22px] flex flex-col items-center"
+          >
+            <img src={chair} alt="" />
+            <h4 className="font-semibold text-xl font-fontNarrow mt-[18px]">
+              Chair
+            </h4>
+          </div>
+          {/* card 3 */}
+          <div
+            style={{ boxShadow: "0px 1px 13px 0px rgba(46, 49, 146, 0.09)" }}
+            className="pt-[23px] pb-[15px] px-[20px] bg-gradient-to-b from-[#BDEFDE] to-[#70CEB1] rounded-[22px] flex flex-col items-center"
+          >
+            <img src={bed} alt="" />
+            <h4 className="font-semibold text-xl font-fontNarrow mt-[18px]">
+              Bed
+            </h4>
+          </div>
         </div>
       </div>
     </>
